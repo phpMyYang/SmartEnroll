c0d09f303ba7ae9e122fd455adef2ad7137596f4
phpMyYang <jeremiahoccupation@gmail.com>
2026-01-30 17:46:04 +0800

    SmartEnroll: Add 100 for SHOW: sa Files na admin/students, staff/students & staff/subjects.

--- resources/js/pages/staff/Subjects.jsx
+++ resources/js/pages/staff/Subjects.jsx
@@ -155,6 +155,7 @@ export default function StaffSubjects() {
                             <option value="10">10</option>
                             <option value="25">25</option>
                             <option value="50">50</option>
+                            <option value="100">100</option>
                         </select>
                     </div>
                     <div className="input-group" style={{ maxWidth: "300px" }}>
